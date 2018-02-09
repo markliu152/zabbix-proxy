36ba6e9cc945609342d2ed7444e62c8953c8948a
markliu152 <1099134352@qq.com>
2018-02-09 23:15:40 +0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# zabbix-proxy
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: zabbix-proxy
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
