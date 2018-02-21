adae15a3ae9d1c0242576e896456ac8fba257f0d
markliu152 <34978122+markliu152@users.noreply.github.com>
2018-02-22 01:27:58 +0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,24 +4,25 @@
 #
 # Copyright:: 2018, Mark Liu, All Rights Reserved.
 
-include_recipe 'zabbix::release'
-include_recipe 'zabbix::firewall'
+include_recipe 'zabbix1::release'
+include_recipe 'zabbix1::firewall'
+include_recipe 'zabbix-proxy::mariadb'
 
 package 'zabbix-proxy'do
+  action :install
+end
+
+bash 'openssl_psk' do
+  code <<-EOH
+    sh -c "openssl rand -hex 32 > /etc/zabbix/zabbix_proxy.psk"
+  EOH
+end
 
 template '/etc/zabbix/zabbix_proxy.conf' do
   source 'proxy.erb'
   owner 'root'
   group 'root'
   mode '0644'
-end
-
-include_recipe 'zabbix-proxy::mariadb'
-
-bash 'sh_openssl_rand' do
-  code <<-EOF
-    sh -c "openssl rand -hex 32 > /etc/zabbix/zabbix_proxy.psk"
-  EOF
 end
 
 include_recipe 'zabbix1::selinux'
