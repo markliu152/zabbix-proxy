ff3fd2a0844749095978eb90209238af2c3448d4
markliu152 <34978122+markliu152@users.noreply.github.com>
2018-02-22 01:26:35 +0800

    Update mariadb.rb

--- recipes/mariadb.rb
+++ recipes/mariadb.rb
@@ -1,11 +1,19 @@
-include_recipe 'zabbix1::mysql'
-
-bash 'zcat_schema' do
-  user 'root'
-    cwd node['zabbix']['schema']['path'].to_s
-    code <<-"EOF"
-      zcat #{node['zabbix']['schema']['path']}/schema.sql.gz | mysql -u#{node['zabbix']['mysql']['user_name']} -p#{node['zabbix']['mysql']['user_password']} #{node['zabbix']['mysql']['db_name']}
-    EOF
-    not_if "mysqlshow --user=root --password=#{node['zabbix']['mysql']['root_password']} #{node['zabbix']['mysql']['db_name']}"
+package 'mariadb'
+package 'mariadb-server'
+service 'mariadb' do
+  action [:enable, :start]
 end
 
+bash "create_database" do
+  code <<-EOH
+    mysql -uroot -e "create database #{node['zabbix']['proxy']['db_name']} character set utf8 collate utf8_bin;"
+    mysql -uroot -e "grant all privileges on #{node['zabbix']['proxy']['db_name']}.* to #{node['zabbix']['proxy']['user_name']}@localhost identified by '#{node['zabbix']['proxy']['user_password']}';"
+  EOH
+  not_if "mysql -uroot -e'show databases;' | grep #{node['zabbix']['proxy']['db_name']}"
+end
+
+bash 'extract_data' do
+  code <<-EOH
+    zcat #{node['path']} | mysql -u#{node['zabbix']['proxy']['user_name']} -p#{node['zabbix']['proxy']['user_password']} #{node['zabbix']['proxy']['db_name']}
+  EOH
+end
